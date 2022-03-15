85830034c4ec5614b4b97ae27d9928a1e51507d2
747540 <arvindkumar9648901008@gmail.com>
2022-03-15 23:33:12 +0530

    Commit By Arvind

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 double lat = 1.0;
 double lon = 1.0;
-const String apiKey = "d37c4a7a5b8d83e8024babdde2dbebf1";
+const String apiKey = "Your OpenWeatherMap API Key";
 String url =
     "https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&units=metric&appid=$apiKey";
